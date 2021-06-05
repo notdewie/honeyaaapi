2eae33b25f0114c7fe78144814e74191506d20d5
TrDayken <tranhdayken@gmail.com>
2021-06-05 21:41:12 +0700

    [UPDATE] Create full table

--- server/admin.py
+++ server/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User, Person, SwipePerson
+from .models import User, Person, SwipePerson, Interest, Oriented
 
 admin.site.register(User)
 admin.site.register(Person) 
 admin.site.register(SwipePerson)
+admin.site.register(Interest)
+admin.site.register(Oriented) 
 
 # Register your models here.
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
 from django.db.models.fields.related import OneToOneField
 
 # class Players (models.Model):
@@ -7,6 +8,7 @@ from django.db.models.fields.related import OneToOneField
 #     namePlayer = models.TextField(max_length=100)
 #     agePlayer = models.IntegerField()
 
+# user table
 class User (models.Model):
     userName = models.CharField(max_length=20)
     userPassword = models.CharField(max_length=20)
@@ -14,6 +16,7 @@ class User (models.Model):
     def __str__(self):
         return self.userName
 
+# person table
 class Person (models.Model):
     personName = models.CharField(max_length=20)
 
@@ -24,12 +27,33 @@ class Person (models.Model):
     )
 
     swipePerson = models.ManyToManyField('self', through='SwipePerson', symmetrical=False)
+    interested = models.ManyToManyField('Interest')
+    oriented = models.ForeignKey('Oriented', on_delete=models.SET_NULL, null=True)
+
 
     def __str__(self):
         return self.personName
 
+# Person to Person third table
 class SwipePerson (models.Model):
-    fromPerson = models.ForeignKey(Person, related_name='from_person' ,on_delete=models.CASCADE)
-    toPerson = models.ForeignKey(Person, related_name='to_person', on_delete=models.CASCADE)
+    fromPerson = models.ForeignKey(Person, related_name='from_person' ,on_delete=models.SET_NULL, null=True)
+    toPerson = models.ForeignKey(Person, related_name='to_person', on_delete=models.SET_NULL, null=True)
 
     liked = models.BooleanField()
+
+    def __str__(self):
+        return "{} to {}".format(self.fromPerson, self.toPerson)
+
+# Interest table
+class Interest (models.Model):
+    interestName = models.CharField(max_length=20)
+
+    def __str__ (self):
+        return self.interestName
+
+# sex oriented table
+class Oriented (models.Model):
+    orientedType = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.orientedType
